5db6b7af881dc838937fd36a3897f65780e50d9e
Abdulhakeem Bello <abdulhakeemb40@gmail.com>
2023-03-09 17:44:39 +0000

    Honey comb Span intrumentation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -20,13 +20,19 @@ from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
 
 # Initialize tracing and an exporter that can send data to Honeycomb
 #Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+#show trace in STDOUT for debugging
+#local_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(local_processor)
+
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 

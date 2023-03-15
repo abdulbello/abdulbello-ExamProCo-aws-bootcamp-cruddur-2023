6e877d58a05f2768de0c90a0599a53216b97220e
Abdulhakeem Bello <112012120+abdulbello@users.noreply.github.com>
2023-03-15 15:33:15 +0100

    updated script

--- run_sonarqube_script.sh
+++ run_sonarqube_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --net=host -v ${PWD}:/sonarqube-flask sonarsource/sonar-scanner-cli sonar-scanner -D sonar.projectBaseDir=/sonarqube-flask
+/root/.sonar/sonar-scanner-4.7.0.2747-macosx/lib java -jar sonar-scanner-cli-4.7.0.2747.jar  -Dsonar.projectKey=UEL_Thesis   -Dsonar.sources=.   -Dsonar.host.url=http://192.168.168.115:9000

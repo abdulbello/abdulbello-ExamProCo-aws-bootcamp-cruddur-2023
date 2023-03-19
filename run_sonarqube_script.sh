00531e4767bebbfca7197d45acbfa1a75349f99e
Abdulhakeem Bello <112012120+abdulbello@users.noreply.github.com>
2023-03-19 18:21:00 +0100

    Delete run_sonarqube_script.sh

--- run_sonarqube_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar /sonar-scanner-4.7.0.2747-macosx/lib/sonar-scanner-cli-4.7.0.2747.jar  -Dsonar.projectKey=UEL_Thesis   -Dsonar.sources=./backend-flask/   -Dsonar.host.url=http://172.20.10.3:9000

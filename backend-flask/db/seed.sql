724ab2aa3edc18c21c273c371fbb579395b32d9a
Abdulhakeem Bello <abdulhakeemb40@gmail.com>
2023-04-05 12:21:42 +0000

    implemented local ddb seed and scan

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,10 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK'),
-  ('Abdul Bello', 'abello' , 'MOCK');
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Londo Mollari', 'lmollari@centari.com','londo','MOCK'),
+  ('Abdul Bello', 'abdulhakeemb40@gmail.com', 'abello' , 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES

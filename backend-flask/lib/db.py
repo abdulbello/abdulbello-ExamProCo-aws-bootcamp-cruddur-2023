40adb47b18c18e0b8d17b3c842f77c43fcf41a24
Abdulhakeem Bello <abdulhakeemb40@gmail.com>
2023-04-06 17:15:41 +0000

    debugging postgres connection issue

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -40,8 +40,6 @@ class Db:
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
     print(sql,params)
-
-    
   def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql,params)
 
@@ -59,8 +57,6 @@ class Db:
           return returning_id
     except Exception as err:
       self.print_sql_err(err)
-
-
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
     self.print_sql('array',sql,params)
@@ -71,8 +67,6 @@ class Db:
         cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         return json[0]
-
-
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
 
@@ -88,8 +82,6 @@ class Db:
           "{}"
         else:
           return json[0]
-
-#return a single value
   def query_value(self,sql,params={}):
     self.print_sql('value',sql,params)
     with self.pool.connection() as conn:
@@ -97,8 +89,6 @@ class Db:
         cur.execute(sql,params)
         json = cur.fetchone()
         return json[0]
-
-
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -106,8 +96,6 @@ class Db:
     ) object_row);
     """
     return sql
-
-
   def query_wrap_array(self,template):
     sql = f"""
     (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
@@ -115,8 +103,6 @@ class Db:
     ) array_row);
     """
     return sql
-
-    
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
